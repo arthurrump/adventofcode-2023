ae85257bbb42dbada92e01de3f9b6448a6fd79d7
Arthur Rump <hello@arthurrump.com>
2023-12-10 14:00:44 +0100

    Day05 part 2

--- Day05/Program.fs
+++ Day05/Program.fs
@@ -1,22 +1,70 @@
 ﻿open System
 open System.IO
 
+type Range =
+    { /// Inclusive start-index
+      Start: int64
+      /// Exclusive end-index
+      End: int64 }
+
+module Range =
+    let containsValue x range =
+        range.Start <= x && x < range.End
+
+    let contains range1 range2 =
+        range2.Start <= range1.Start && range1.End <= range2.End
+    
+    let overlaps range1 range2 =
+        not (range1.Start >= range2.End) && not (range1.End <= range2.Start)
+
+    let tryIntersect range1 range2 =
+        if overlaps range1 range2 
+        then Some { Start = max range1.Start range2.Start
+                    End = min range1.End range2.End }
+        else None
+
+    let shift x range =
+        { Start = range.Start + x
+          End = range.End + x }
+
+type Ranges =
+    Range list
+
+// module Ranges =
+//     let simplify ranges =
+//         ranges
+//         |> List.sortBy _.Start
+//         |> List.fold ()
+
 type Mapping =
-    { DestinationRangeStart: int64
-      SourceRangeStart: int64
-      RangeLength: int64 }
+    { Source: Range
+      Shift: int64 }
 
 module Mapping =
     let parse (line: string) =
         let [| dest; src; len |] = line.Split(" ")
-        { DestinationRangeStart = Int64.Parse dest
-          SourceRangeStart = Int64.Parse src
-          RangeLength = Int64.Parse len }
+        let dest = Int64.Parse dest
+        let src = Int64.Parse src
+        let len = Int64.Parse len
+        { Source = { Start = src; End = src + len }
+          Shift = dest - src }
 
-    let tryMap mapping source =
-        if mapping.SourceRangeStart <= source && source < mapping.SourceRangeStart + mapping.RangeLength
-        then Some (source - mapping.SourceRangeStart + mapping.DestinationRangeStart)
+    let tryMap mapping source =                                                                                                                                                       
+        if mapping.Source |> Range.containsValue source
+        then Some (source + mapping.Shift)
         else None
+
+    let mapRange mapping source =
+        match Range.tryIntersect mapping.Source source with
+        | Some intersection ->
+            let unmapped = 
+                [ if intersection.Start > source.Start then
+                    { Start = source.Start; End = intersection.Start }
+                  if intersection.End < source.End then
+                    { Start = intersection.End; End = source.End } ]
+            Some (Range.shift mapping.Shift intersection), unmapped
+        | None ->
+            None, [ source ]
 
 type Map =
     Mapping list
@@ -28,6 +76,16 @@ module Map =
     let map map source =
         tryMap map source
         |> Option.defaultValue source
+
+    let mapRanges (map: Map) (sources: Ranges) =
+        List.fold (fun (mapped, unmapped) mapping -> 
+            let newMapped, newUnmapped =
+                unmapped 
+                |> List.map (Mapping.mapRange mapping)
+                |> List.unzip
+            (mapped @ List.choose id newMapped, List.concat newUnmapped)
+        ) ([], sources) map
+        ||> List.append
 
 type Almanac =
     { Seeds: int64 list
@@ -62,12 +120,12 @@ let part1 () =
 
 printfn "Part 1: %d" (part1 ())
 
-// Way to slow...
-// let part2 () =
-//     almanac.Seeds 
-//     |> List.chunkBySize 2
-//     |> List.collect (fun [ start; len ] -> List.init (int len) (fun i -> start + int64 i))
-//     |> List.map (fun seed -> almanac.Maps |> List.fold (fun n map -> Map.map map n) seed)
-//     |> List.min
+let part2 () =
+    almanac.Seeds 
+    |> List.chunkBySize 2
+    |> List.map (fun [ start; len ] -> { Start = start; End = start + len })
+    |> fun seeds -> List.fold (fun n map -> Map.mapRanges map n) seeds almanac.Maps
+    |> List.map _.Start
+    |> List.min
 
-// printfn "Part 2: %d" (part2 ())
+printfn "Part 2: %A" (part2 ())
