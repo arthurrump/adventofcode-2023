1502646334a5a1db75857a43b93aaa1872d9dfac
Arthur Rump <hello@arthurrump.com>
2023-12-10 17:33:47 +0100

    Day09

---
+++ Day09/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.IO
+
+let readings =
+    File.ReadAllLines("input.txt")
+    |> Array.map (fun line -> line.Split(" ") |> Array.map Int32.Parse)
+
+let differences =
+    Array.pairwise >> Array.map (fun (left, right) -> right - left)
+
+let rec extrapolate arr =
+    let diff = differences arr
+    if Array.forall (fun d -> d = 0) diff
+    then Array.last arr
+    else Array.last arr + (extrapolate diff)
+
+let part1 () =
+    readings
+    |> Array.map extrapolate
+    |> Array.sum
+
+printfn "Part 1: %d" (part1 ())
+
+let part2 () =
+    readings
+    |> Array.map (Array.rev >> extrapolate)
+    |> Array.sum
+
+printfn "Part 2: %d" (part2 ())
