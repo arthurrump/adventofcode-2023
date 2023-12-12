9bcb94d7dcfecd4d47d0344c4358aaf82de8e245
Arthur Rump <hello@arthurrump.com>
2023-12-12 22:36:29 +0100

    Day12 failed experiment

--- Day12/Program.fs
+++ Day12/Program.fs
@@ -1,11 +1,12 @@
 ﻿open System
+open System.Collections.Generic
 open System.IO
 
 type Condition = Operational | Damaged | Unknown
 
 type ConditionRecord =
-    { SpringConditions: Condition list
-      DamagedGroups: int list }
+    { Springs: Condition list
+      Chunks: int list }
 
 let parseCondition = function
     | '.' -> Operational
@@ -14,74 +15,106 @@ let parseCondition = function
     | _ -> failwith "Invalid condition"
 
 let parseRecord (line: string) =
-    let [| conditions; groups |] = line.Split(" ")
-    { SpringConditions = conditions.ToCharArray() |> Array.map parseCondition |> Array.toList 
-      DamagedGroups = groups.Split(",") |> Array.map Int32.Parse |> Array.toList }
+    let [| conditions; chunks |] = line.Split(" ")
+    { Springs = conditions.ToCharArray() |> Array.map parseCondition |> Array.toList 
+      Chunks = chunks.Split(",") |> Array.map Int32.Parse |> Array.toList }
 
 let records =
-    File.ReadAllLines("input.txt")
+    File.ReadAllLines("example.txt")
     |> Array.map parseRecord
 
-module List =
-    let rec hasPrefix xs ys =
-        match xs, ys with
-        | x::xs, y::ys -> x = y && hasPrefix xs ys
-        | [], _ -> true
-        | _::_, [] -> false
 
-let countDamagedGroups conditions =
-    let groups =
-        List.fold (fun (group::groups) condition ->
-            match condition with
-            | Operational | Unknown when group = 0 ->
-                group::groups
-            | Operational | Unknown ->
-                0::group::groups
-            | Damaged -> 
-                (group + 1)::groups
-        ) [ 0 ] conditions
-    match groups with
-    | 0::rest -> List.rev rest
-    | _ -> List.rev groups
+// Based on https://www.youtube.com/watch?v=GVcER1GavNQ
+let count =
+    let cache = new Dictionary<_, _>()
 
-let rec validArrangements conditionsValidUpto groupsValidCount record =
-    match record.SpringConditions |> List.tryFindIndex (fun c -> c = Unknown) with
-    | Some unknownIndex ->
-        let lastOperationalIndex = 
-            record.SpringConditions[..unknownIndex - 1] 
-            |> List.tryFindIndexBack (fun c -> c = Operational)
-            |> Option.defaultValue 0
-        let newDamagedGroups = 
-            countDamagedGroups record.SpringConditions[conditionsValidUpto..lastOperationalIndex - 1]
-        if record.DamagedGroups |> List.skip groupsValidCount |> List.hasPrefix newDamagedGroups then
-            let groupsValidCount = groupsValidCount + (List.length newDamagedGroups)
-            seq {
-                yield! validArrangements lastOperationalIndex groupsValidCount { record with SpringConditions = record.SpringConditions |> List.updateAt unknownIndex Operational }
-                yield! validArrangements lastOperationalIndex groupsValidCount { record with SpringConditions = record.SpringConditions |> List.updateAt unknownIndex Damaged }
-            }
-        else
-            Seq.empty
-    | None ->
-        if countDamagedGroups record.SpringConditions = record.DamagedGroups
-        then Seq.singleton record.SpringConditions
-        else Seq.empty
+    let rec count = function
+        // There are no more springs and we covered all chunks
+        | [], [], _
+        // There are no more springs and the final chunk zeroed out
+        | [], [ 0 ], _ ->
+            // So we're done and this is valid
+            1
+
+        // There are no more springs, but there are still one or more chunks left
+        | [], _, _ ->
+            // Then this is invalid
+            0
+
+        // There are no more chunks and no Damaged springs left
+        | springs, [], _ when springs |> List.forall (fun s -> s <> Damaged) ->
+            // Then this is valid
+            1
+        // No more chunks, but some of the next chunks are Damaged
+        | _, [], _ ->
+            // Then this is invalid
+            0
+        
+        // The next is operational and the chunk we are in is finished
+        | Operational::springs, 0::chunks, true
+        // The next is operational and we are not in a chunk
+        | Operational::springs, chunks, false ->
+            // Then go to the next
+            countMemo (springs, chunks, false)
+        // The next is operational, but we are in a non-zeroed-out chunk
+        | Operational::_, _, true ->
+            // Then this is not a valid option
+            0
+        
+        // The next chunk is zero while we are not in a chunk
+        | _, 0::_, false ->
+            // That should not be possible
+            failwith "Impossible case"
+
+        // The next is damaged, we are in a chunk, but the current chunk zeroed out
+        | Damaged::_, 0::_, true ->
+            // That's not valid, because the current chunk is larger
+            0
+        // The next is damaged, the current chunk has room to play
+        | Damaged::springs, n::chunks, _ ->
+            // Then go to the next
+            countMemo (springs, n-1::chunks, true)
+
+        // The next is unkown and we are in a zeroed out chunk
+        | Unknown::springs, 0::chunks, true ->
+            // Then this should be Operational and we can just count on as such
+            countMemo (springs, chunks, false)
+        // The next spring is unknown and we are in a chunk that is not yet 0
+        | Unknown::springs, n::chunks, true ->
+            // Then this should be Damaged and we can just count on as such
+            countMemo (springs, n-1::chunks, true)
+        // The next spring is unknown, and we could start a new chunk
+        | Unknown::springs, n::chunks, false ->
+            // Then we may choose this as Operational
+            countMemo (springs, n::chunks, false)
+            // or as Damaged
+            + countMemo (springs, n-1::chunks, true)
+
+    and countMemo (springs, chunks, inChunk) = 
+        match cache.TryGetValue((springs, chunks, inChunk)) with
+        | true, res -> 
+            res
+        | false, _ ->
+            let result = countMemo (springs, chunks, inChunk)
+            cache.Add((springs, chunks, inChunk), result)
+            result
+                
+    fun record -> countMemo (record.Springs, record.Chunks, false)
 
 let part1 () =
     records
-    |> Array.map (validArrangements 0 0 >> Seq.length)
+    |> Array.map count
     |> Array.sum
 
 printfn "Part 1: %A" (part1 ())
 
 let unfold record =
-    { SpringConditions = List.concat (List.replicate 4 (record.SpringConditions @ [ Unknown ])) @ record.SpringConditions
-      DamagedGroups = List.concat (List.replicate 5 record.DamagedGroups) }
-
-let tee f x = f x; x
+    { Springs = List.concat (List.replicate 4 (record.Springs @ [ Unknown ])) @ record.Springs
+      Chunks = List.concat (List.replicate 5 record.Chunks) }
 
 let part2 () =
     records
-    |> Array.Parallel.map (unfold >> validArrangements 0 0 >> Seq.length >> tee (printfn "%d") >> bigint)
+    |> Array.map (unfold >> count >> bigint)
     |> Array.sum
 
 printfn "Part 2: %A" (part2 ())
