443079c267d3e3d9946af8cf1cd823ce325216aa
Arthur Rump <hello@arthurrump.com>
2023-12-17 16:50:11 +0100

    Day17 part 2

--- Day17/Program.fs
+++ Day17/Program.fs
@@ -10,18 +10,6 @@ let city =
 let maxY = Array2D.length1 city - 1
 let maxX = Array2D.length2 city - 1
 
-let cityGraph =
-    [ for y = 0 to maxY do
-        for x = 0 to maxX do
-            let edges =
-                [ if y > 0 then (y - 1, x)
-                  if y < maxY then (y + 1, x)
-                  if x > 0 then (y, x - 1)
-                  if x < maxX then (y, x + 1) ]
-                |> List.map (fun (ny, nx) -> city[ny, nx], (ny, nx)) 
-            (y, x), edges ]
-    |> Map.ofList
-
 type Direction = North | East | South | West
 
 module Direction =
@@ -45,7 +33,7 @@ type State =
 
 let neighbours state =
     let newDirections =
-        if state.StraightMoves < 2
+        if state.StraightMoves < 3
         then Direction.straightOrTurns state.Direction
         else Direction.turns state.Direction
     [ for dir in newDirections do
@@ -53,23 +41,22 @@ let neighbours state =
         if 0 <= y && y <= maxY && 0 <= x && x <= maxX then
             { Location = (y, x)
               Direction = dir
-              StraightMoves = if dir = state.Direction then state.StraightMoves + 1 else 0 } ]
+              StraightMoves = if dir = state.Direction then state.StraightMoves + 1 else 1 } ]
 
-let dijkstra start dest (city: int[,]) =
-    let unvisited = PriorityQueue<State, int>()
-    let distances = Dictionary<State, int>()
-    let paths = Dictionary<State, State list>()
-    let mutable current = { Location = start; Direction = East; StraightMoves = 0 }
-    distances.Add(current, 0)
-    unvisited.Enqueue(current, 0)
-    paths.Add(current, [])
-    while current.Location <> dest do
+let dijkstra starts isDest neighbours cost =
+    let unvisited = PriorityQueue<'n, int>()
+    let distances = Dictionary<'n, int>()
+    let paths = Dictionary<'n, 'n list>()
+    let mutable current = starts |> List.head
+    for start in starts do
+        distances.Add(start, 0)
+        unvisited.Enqueue(start, 0)
+        paths.Add(start, [])
+    while not (isDest current) do
         current <- unvisited.Dequeue()
         let dist = distances[current]
         for neighbour in neighbours current do
-            let (ny, nx) = neighbour.Location
-            let edge = city[ny, nx]
-            let dist = dist + edge
+            let dist = dist + cost neighbour
             match distances.TryGetValue(neighbour) with
             | true, d ->
                 if dist < d then
@@ -83,22 +70,71 @@ let dijkstra start dest (city: int[,]) =
                 
     distances[current], List.rev (current::paths[current])
 
-let part1 () =
-    let heatLoss, path = dijkstra (0, 0) (maxY, maxX) city
+let printPath path =
+    let pathMap = 
+        path 
+        |> List.map (fun state -> state.Location, state.Direction) 
+        |> Map.ofList
+    
+    let colors = [| ConsoleColor.White; ConsoleColor.Yellow; ConsoleColor.DarkYellow; ConsoleColor.Magenta; ConsoleColor.Red |]
+    for y = 0 to maxY do
+        for x = 0 to maxX do
+            let v = city[y, x]
+            let color = colors[v / 2]
+            match Map.tryFind (y, x) pathMap with
+            | None -> 
+                Console.ForegroundColor <- color
+                printf "%d" v
+            | Some dir ->
+                Console.BackgroundColor <- color
+                Console.ForegroundColor <- ConsoleColor.Black
+                match dir with
+                | North -> '^'
+                | East -> '>'
+                | West -> '<'
+                | South -> 'v'
+                |> printf "%c"
+            Console.ResetColor()
+        printfn ""
 
-    // let pathMap = path |> List.map (fun state -> state.Location, state.Direction) |> Map.ofList
-    // for y = 0 to maxY do
-    //     for x = 0 to maxX do
-    //         Map.tryFind (y, x) pathMap
-    //         |> function
-    //            | None -> char city[y, x] + '0'
-    //            | Some North -> '^'
-    //            | Some East -> '>'
-    //            | Some West -> '<'
-    //            | Some South -> 'v'
-    //         |> printf "%c"
-    //     printfn ""
+let part1 () =
+    let heatLoss, path = 
+        dijkstra 
+            [ for dir in [ East; South ] -> { Location = (0, 0); Direction = dir; StraightMoves = 1 } ]
+            (fun state -> state.Location = (maxY, maxX))
+            neighbours
+            (fun state -> let (y, x) = state.Location in city[y, x])
+
+    printPath path
 
     heatLoss
 
 printfn "Part 1: %A" (part1 ())
+
+let ultraNeighbours state =
+    let newDirections =
+        if state.StraightMoves < 4
+        then [ state.Direction ]
+        elif state.StraightMoves < 10
+        then Direction.straightOrTurns state.Direction
+        else Direction.turns state.Direction
+    [ for dir in newDirections do
+        let (y, x) = Direction.move state.Location dir
+        if 0 <= y && y <= maxY && 0 <= x && x <= maxX then
+            { Location = (y, x)
+              Direction = dir
+              StraightMoves = if dir = state.Direction then state.StraightMoves + 1 else 1 } ]
+
+let part2 () =
+    let heatloss, path =
+        dijkstra
+            [ for dir in [ East; South ] -> { Location = (0, 0); Direction = dir; StraightMoves = 1 } ]
+            (fun state -> state.Location = (maxY, maxX) && state.StraightMoves >= 4)
+            ultraNeighbours
+            (fun state -> let (y, x) = state.Location in city[y, x])
+    
+    printPath path
+
+    heatloss
+
+printfn "Part 2: %A" (part2 ())
