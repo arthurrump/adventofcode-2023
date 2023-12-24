76b0088671e714aa0cba600dd6d1f86ffe29dd34
Arthur Rump <hello@arthurrump.com>
2023-12-24 15:42:38 +0100

    Day24 part 2

--- Day24/Program.fs
+++ Day24/Program.fs
@@ -1,4 +1,5 @@
 ﻿open MathNet.Numerics.LinearAlgebra
+open Microsoft.Z3
 open System
 open System.IO
 
@@ -51,4 +52,34 @@ let part1 () =
     intersectingPairsWithin 2 200000000000000. 400000000000000. hailstones
     |> Array.length
 
-printfn "Part 1: %A" (part1 ())
+// printfn "Part 1: %A" (part1 ())
+
+let part2 () =
+    use ctx = new Context()
+
+    let rockP = [| for i = 0 to 2 do ctx.MkRealConst($"rock_p{i}") |]
+    let rockV = [| for i = 0 to 2 do ctx.MkRealConst($"rock_v{i}") |]
+
+    let goal = ctx.MkGoal()
+    for i, (hailP, hailV) in Array.indexed hailstones |> Array.take 3 do
+        let t = ctx.MkRealConst($"t_{i}")
+        goal.Assert(ctx.MkGe(t, ctx.MkReal(0)))
+        for dim = 0 to 2 do
+            goal.Assert(ctx.MkEq(
+                rockP[dim] + t * rockV[dim],
+                ctx.MkReal(string hailP[dim]) + t * ctx.MkReal(string hailV[dim])
+            ))
+    
+    let solver = ctx.MkSimpleSolver()
+    solver.Assert(goal.Formulas)
+
+    printfn "%O" solver
+
+    assert (solver.Check() = Status.SATISFIABLE)
+
+    Array.map (fun expr -> expr :> ArithExpr) rockP
+    |> Array.reduce (+)
+    |> solver.Model.Eval
+    |> string
+
+printfn "Part 2: %A" (part2 ())
