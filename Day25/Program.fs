96756c8a0858a1ff71b2964dba274291daf5718a
Arthur Rump <hello@arthurrump.com>
2023-12-25 15:41:35 +0100

    Day25!

--- Day25/Program.fs
+++ Day25/Program.fs
@@ -1,26 +1,26 @@
 ﻿open System
+open System.Collections.Generic
 open System.IO
 
 let parseLine (line: string) =
     let [| from; tos |] = line.Split(": ")
     let tos = tos.Split(" ")
-    from, tos
+    from, Array.toList tos
 
 let wires = 
     File.ReadAllLines("input.txt")
-    |> Array.collect (fun line ->
-        let from, tos = parseLine line
-        tos |> Array.map (fun to' -> from, to')
-    )
+    |> Array.map parseLine
+    |> Map.ofArray
 
-// let wireGraph = 
-//     [ for KeyValue (from, tos) in wires do
-//         for to' in tos do
-//             (from, to')
-//             (to', from) ]
-//     |> List.groupBy fst
-//     |> List.map (fun (from, edges) -> from, List.map (fun (_, to') -> to', 1) edges)
-//     |> Map.ofList
+let wireGraph = 
+    [ for KeyValue (from, tos) in wires do
+        for to' in tos do
+            (from, to')
+            (to', from) ]
+    |> List.distinct
+    |> List.groupBy fst
+    |> List.map (fun (from, edges) -> from, List.map snd edges)
+    |> Map.ofList
 
 let dot (name: string) (graph: Map<string, string list>) (tw: TextWriter) = 
     tw.Write("graph "); tw.Write(name); tw.WriteLine(" {")
@@ -30,65 +30,57 @@ let dot (name: string) (graph: Map<string, string list>) (tw: TextWriter) =
     tw.WriteLine("}")
     tw.Flush()
 
-let contractable graph =
-    graph
-    |> Array.map (fun (a, b) -> Set.singleton a, (a, b), Set.singleton b)
+let bfsPath (start: 'node) (dest: 'node) (isAvailable: 'node * 'node -> bool) (graph: Map<'node, 'node list>) =
+    let frontier = Queue<'node>([ start ])
+    let paths = Dictionary<'node, 'node list>([ KeyValuePair (start, []) ])
+    while frontier.Count > 0 && not (paths.ContainsKey dest) do
+        let current = frontier.Dequeue()
+        for neighbour in Map.tryFind current graph |> Option.defaultValue List.empty do
+            if not (paths.ContainsKey neighbour) && isAvailable (current, neighbour) then
+                frontier.Enqueue(neighbour)
+                paths.Add(neighbour, current :: paths[current])
+    match paths.TryGetValue(dest) with
+    | true, path -> Some (List.rev path)
+    | false, _ -> None
 
-// https://en.wikipedia.org/wiki/Karger%27s_algorithm#Karger%E2%80%93Stein_algorithm
-let contractEdge (a, edge, b) graph =
-    let node = Set.union a b
-    graph
-    |> Array.filter (fun (a', _, b') -> not (a = a' && b = b' || a = b' && b = a'))
-    |> Array.map (fun (a', edge', b') -> 
-        if a' = a || a' = b then node, edge', b'
-        elif b' = a || b' = b then a', edge', node
-        else a', edge', b'
-    )
+let bfsDiscovery (start: 'node) (isAvailable: 'node * 'node -> bool) (graph: Map<'node, 'node list>) =
+    let frontier = Queue<'node>([ start ])
+    let discovered = HashSet<'node>([ start ])
+    while frontier.Count > 0 do
+        let current = frontier.Dequeue()
+        for neighbour in Map.tryFind current graph |> Option.defaultValue List.empty do
+            if not (discovered.Contains neighbour) && isAvailable (current, neighbour) then
+                frontier.Enqueue(neighbour)
+                assert discovered.Add(neighbour)
+    set discovered
 
-let nodes graph = 
-    seq {
-        for a, _, b in graph do
-            yield a
-            yield b
-    } 
-    |> Seq.distinct
-    |> Seq.length
+let edmondsKarp (source: 'node) (sink: 'node) (capacity: 'node * 'node -> int) (graph: Map<'node, 'node list>) =
+    let rec run maxFlow flow =
+        let getFlow edge = 
+            flow |> Map.tryFind edge |> Option.defaultValue 0
+        match bfsPath source sink (fun edge -> getFlow edge < capacity edge) graph with
+        | Some augmentingPath ->
+            let augmentingEdges = augmentingPath |> List.pairwise
+            let addFlow = 
+                augmentingEdges
+                |> Seq.map (fun edge -> capacity edge - getFlow edge)
+                |> Seq.min
+            let flow =
+                augmentingEdges
+                |> List.fold (fun flow edge -> Map.add edge (getFlow edge + addFlow) flow) flow
+            run (maxFlow + addFlow) flow
+        | None ->
+            maxFlow, flow
+    run 0 Map.empty
 
-let contractRandom t graph =
-    let n = nodes graph - t
-    let rec contractRandomTimes n graph =
-        if n <= 0 then
-            graph
-        else
-            let i = Random.Shared.Next(Array.length graph)
-            let e = graph[i]
-            contractRandomTimes (n - 1) (contractEdge e graph)
-    contractRandomTimes n graph
-
-let rec fastmincut graph =
-    let nodes = nodes graph
-    if nodes <= 6 then
-        contractRandom 2 graph
-    else
-        let t = int (ceil (1. + float nodes / sqrt 2.))
-        let g1 = fastmincut (contractRandom t graph)
-        let g2 = fastmincut (contractRandom t graph)
-        [ g1; g2 ]
-        |> List.minBy Array.length
-
-let rec repeatedMincut expectedMincut graph =
-    let res = contractRandom 2 graph
-    if Array.length res > expectedMincut then 
-        printfn "Found minimum cut of %d edges. Trying again." (Array.length res)
-        repeatedMincut expectedMincut graph
-    else
-        res
+let minCut source sink (graph: Map<'node, 'node list>) =
+    let _, flow = edmondsKarp source sink (fun _ -> 1) graph
+    let nodes = set (Map.keys graph)
+    let sourceComponent = bfsDiscovery source (fun edge -> not (flow |> Map.containsKey edge)) graph
+    sourceComponent, nodes - sourceComponent
 
 let part1 () =
-    let left, _, right =
-        contractable wires
-        |> repeatedMincut 3
-        |> Array.head
+    let left, right = minCut "str" "mmj" wireGraph
     Set.count left * Set.count right
 
 printfn "Part 1: %A" (part1 ())
